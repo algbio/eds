9c5f700591fc208559ff4221be233778be087768
Veli Mäkinen <veli.makinen@helsinki.fi>
2025-06-04 13:52:03 +0300

    verbose mode and printing EDS to file

--- msa2eds-mincard.cpp
+++ msa2eds-mincard.cpp
@@ -12,6 +12,7 @@ using namespace std::chrono;
 using namespace std;
 
 const int INF = INT_MAX;
+bool verbose = false;
 
 // Reads sequences from a FASTA file
 vector<string> read_fasta(const string& filename) {
@@ -134,8 +135,12 @@ pair<int, vector<pair<int, int>>> segment_with_rmq(
 }
 
 // Print EDS from segmentation
-void print_eds(const vector<string>& msa, const vector<pair<int, int>>& segments) {
-    cout << "Elastic Degenerate String (EDS):\n";
+void print_eds(const vector<string>& msa, const vector<pair<int, int>>& segments, string out_filename = "") {
+    std::ofstream outFile;
+    if (out_filename.size()==0) 
+        cout << "Elastic Degenerate String (EDS):\n";
+    else
+       outFile.open(out_filename);
     for (const auto& [l, r] : segments) {
         set<string> unique_subs;
         for (const auto& seq : msa) {
@@ -143,13 +148,38 @@ void print_eds(const vector<string>& msa, const vector<pair<int, int>>& segments
             sub.erase(remove(sub.begin(), sub.end(), '-'), sub.end()); // remove gaps
             unique_subs.insert(sub);
         }
-
-        cout << "{ ";
-        for (const auto& s : unique_subs)
-            cout << s << " ";
-        cout << "}";
+        if (out_filename.size()==0) { 
+            cout << "{ ";
+            for (const auto& s : unique_subs)
+                cout << s << " ";
+            cout << "}";
+        }
+        if (outFile) {
+            outFile << "{ ";
+            for (const auto& s : unique_subs)
+                outFile << s << " ";
+            outFile << "}";
+        }         
     }
-    cout << "\n";
+    if (out_filename.size()==0) 
+        cout << "\n";
+    if (outFile)
+        outFile.close();
+}
+
+// Count the total cardinality of sets
+int card_eds(const vector<string>& msa, const vector<pair<int, int>>& segments) {
+    int card = 0;
+    for (const auto& [l, r] : segments) {
+        set<string> unique_subs;
+        for (const auto& seq : msa) {
+            string sub = seq.substr(l - 1, r - l + 1);
+            sub.erase(remove(sub.begin(), sub.end(), '-'), sub.end()); // remove gaps
+            unique_subs.insert(sub);
+        }
+        card += unique_subs.size();
+    }
+    return card;
 }
 
 // Main function
@@ -159,14 +189,15 @@ int main(int argc, char* argv[]) {
     int U = 10;
 
     if (argc<=1) {
-      cout << "Syntax: " << string(argv[0]) << " msa.fasta segment-length-upper-bound (default " << U << ")" << endl;
+      cout << "Syntax: " << string(argv[0]) << " msa.fasta segment-length-upper-bound (default " << U << ") verbose (default 0)" << endl;
       return 0;
     }
-    
+
     filename = string(argv[1]);
     if (argc>2)
       U = atoi(argv[2]);
-
+    if (argc>3)
+      verbose = atoi(argv[3]);
     auto msa = read_fasta(filename);
     if (msa.empty()) {
         cerr << "MSA file is empty or not found.\n";
@@ -177,16 +208,16 @@ int main(int argc, char* argv[]) {
     auto stop_pre = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop_pre-start_pre);
     cout << "Preprocessing took " << duration.count() << " milliseconds" << endl;
-    /*
-    cout << "Meaningful left extensions and heights:\n";
-    for (int y = 1; y < L_y.size(); ++y) {
-        if (L_y[y].empty()) continue;
-        cout << "y = " << y << ":\n";
-        for (size_t j = 0; j < L_y[y].size(); ++j) {
-            cout << "  ℓ: " << L_y[y][j].first << "   h: " << L_y[y][j].second << "\n";
+    if (verbose) {
+        cout << "Meaningful left extensions and heights:\n";
+        for (int y = 1; y < L_y.size(); ++y) {
+            if (L_y[y].empty()) continue;
+            cout << "y = " << y << ":\n";
+            for (size_t j = 0; j < L_y[y].size(); ++j) {
+                cout << "  ℓ: " << L_y[y][j].first << "   h: " << L_y[y][j].second << "\n";
+            }
         }
     }
-    */
     auto start_dp = high_resolution_clock::now();
     auto [cost, segments] = segment_with_rmq(L_y, msa[0].size());
     auto stop_dp = high_resolution_clock::now();
@@ -194,11 +225,15 @@ int main(int argc, char* argv[]) {
     cout << "DP took " << duration.count() << " milliseconds" << endl;
 
     cout << "Minimum segmentation cardinality: " << cost << "\n";
-    cout << "Segments:\n";
-    for (auto [l, r] : segments)
-        cout << "[" << l << "," << r << "] ";
-    cout << "\n";
+    if (verbose) {
+       cout << "Segments:\n";
+       for (auto [l, r] : segments)
+           cout << "[" << l << "," << r << "] ";
+       cout << "\n";
 
-    print_eds(msa, segments);
+       print_eds(msa, segments);
+    }
+    print_eds(msa,segments,filename + ".eds.txt");
+    cout << "Minimum segmentation cardinality after gap removal: " << card_eds(msa, segments) << "\n";
     return 0;
 }
