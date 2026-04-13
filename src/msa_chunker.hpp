291483d1e90c40ff0177685101d5a95b7a046e7e
nrizzo <nrizzo@users.noreply.github.com>
2026-04-13 17:21:18 +0300

    🐛 fetch long sequences from disk (-p)

--- src/msa_chunker.hpp
+++ src/msa_chunker.hpp
@@ -1,6 +1,7 @@
 /*
  * load a FASTA MSA with htslib into memory chunk by chunk, but the chunking
- *   is done automatically, hoping for a linear forward scan of the MSA
+ *   is done automatically, hoping for a linear forward scan of the MSA; since
+ *   we expect all rows to be processed, we only consider vertical MSA slices
  */
 #ifndef MSA_CHUNKER_H
 #define MSA_CHUNKER_H
@@ -23,29 +24,40 @@ using std::cerr, std::endl, std::flush;
 using std::min, std::max;
 using std::filesystem::path, std::filesystem::exists, std::filesystem::last_write_time, std::filesystem::remove;
 
+//#define MSA_CHUNKER_DEBUG
+
 namespace msa_chunker {
-  constexpr int MAX_CHUNK_COLS = 131072;
-  typedef hts_pos_t msa_pos_t;
+  typedef hts_pos_t msa_pos_t; // type for MSA cols, rows index
+
   class msa_chunker {
     private:
+      constexpr static msa_pos_t MIN_CHUNK_COLS = 131072;
+
       faidx_t *idx = NULL;
-      msa_pos_t rows, cols, max_query_length;
+      msa_pos_t rows, cols, max_chunk_cols;
       msa_pos_t chunk_start = std::numeric_limits<msa_pos_t>::max(), chunk_cols = -1;
       vector<string> msa_chunk;
 
+      /*
+       * explicitly load chunk [startcol..startcol+length) into memory
+       */
       void load_chunk(msa_pos_t startcol, msa_pos_t length) {
-        assert(length <= MAX_CHUNK_COLS);
+        assert(length <= max_chunk_cols);
         if (chunk_start <= startcol and startcol + length <= chunk_start + chunk_cols) {
-          //cerr << "DEBUG: not loading chunk (query was [" << startcol << ".." << startcol + length - 1 << "])" << endl;
+#ifdef MSA_CHUNKER_DEBUG
+          cerr << "DEBUG: not loading chunk (query was [" << startcol << ".." << startcol + length - 1 << "])" << endl;
+#endif
           return;
         }
 
         msa_chunk.clear();
-        chunk_start = max((msa_pos_t)0, startcol - max_query_length);
-        chunk_cols = min((msa_pos_t)MAX_CHUNK_COLS, cols - chunk_start);
-        //cerr << "DEBUG: loading chunk [" << chunk_start << ".." << chunk_start + chunk_cols - 1 << "] (0-based) (query was [" << startcol << ".." << startcol + length - 1 << "])" << endl;
+        chunk_start = startcol;
+        chunk_cols = min(max_chunk_cols, cols - chunk_start);
+#ifdef MSA_CHUNKER_DEBUG
+        cerr << "DEBUG: loading chunk [" << chunk_start << ".." << chunk_start + chunk_cols - 1 << "] (0-based) (query was [" << startcol << ".." << startcol + length - 1 << "])" << endl;
+#endif
 
-        for (msa_pos_t r = 0; r < rows; r++) {
+        for (msa_pos_t r = 0; r < rows; ++r) {
           msa_pos_t out_len;
           char *s_str = faidx_fetch_seq64(idx, faidx_iseq(idx, r), chunk_start, chunk_start + chunk_cols - 1, &out_len);
           assert(out_len == chunk_cols);
@@ -56,8 +68,12 @@ namespace msa_chunker {
 
     public:
       msa_chunker() = delete;
-      msa_chunker(const string &fastapath, const int max_qlen) {
-        max_query_length = max_qlen;
+
+      /*
+       * index a given (gzipped) FASTA file
+       */
+      msa_chunker(const string &fastapath, const msa_pos_t max_qlen) {
+        max_chunk_cols = max(max_qlen, MIN_CHUNK_COLS);
         path fastap(fastapath);
         path fastaindex(fastapath + ".fai");
         path gzip_index(fastapath + ".gzi");
@@ -116,14 +132,28 @@ namespace msa_chunker {
       msa_pos_t get_rows() {
         return rows;
       }
+
       msa_pos_t get_cols() {
-        return this->cols;
+        return cols;
       }
 
       string msa_substr(msa_pos_t row, msa_pos_t col, msa_pos_t length) {
         assert(0 <= row and row < rows and 0 <= col and col < cols and col + length <= cols);
-        load_chunk(col, length);
-        return msa_chunk[row].substr(col - chunk_start, length);
+        if (length <= max_chunk_cols) {
+          load_chunk(col, length);
+          return msa_chunk[row].substr(col - chunk_start, length);
+        } else {
+          msa_chunk.clear();
+          chunk_start = std::numeric_limits<msa_pos_t>::max();
+          chunk_cols = -1;
+
+          msa_pos_t out_len;
+          char *s_str = faidx_fetch_seq64(idx, faidx_iseq(idx, row), col, col + length - 1, &out_len);
+          assert(out_len == length);
+          string s(s_str);
+          free(s_str);
+          return s;
+        }
       }
 
       ~msa_chunker() {
@@ -131,4 +161,4 @@ namespace msa_chunker {
       }
   };
 } // msa_chunker
-#endif
+#endif // MSA_CHUNKER_H
